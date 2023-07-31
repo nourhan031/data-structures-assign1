25174f35c3651dd35f21ce6136999a528e965b1b
nourhan031 <nourhanabdallah031@gmail.com>
2023-07-31 23:11:09 +0300

    Student class
    created Student.cpp and Student.h

---
+++ Student.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nourh on 7/31/2023.
+//
+
+#include "Student.h"
---
+++ Student.h
@@ -0,0 +1,35 @@
+//
+// Created by nourh on 7/31/2023.
+//
+
+#ifndef DATA_STRUCTURES_ASSIGN1_STUDENT_H
+#define DATA_STRUCTURES_ASSIGN1_STUDENT_H
+
+
+class Student {
+private:
+    string name;
+    string id;
+    double gpa;
+public:
+    Student(const string& name, string id, double gpa){
+        name = name;
+        id = id;
+        gpa = gpa;
+    }
+    string getName()const{
+        return name;
+    }
+    string getID()const{
+        return id;
+    }
+    double getGPA()const{
+        return gpa;
+    }
+};
+bool operator<(const Student& s1, const Student& s2) {
+    return s1.getName() < s2.getName();
+}
+
+
+#endif //DATA_STRUCTURES_ASSIGN1_STUDENT_H
