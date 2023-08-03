a13661cd1945bf7867c68a83830ff537617a631b
AfnanAbbashar <AfnanAbbashar@gmail.com>
2023-08-03 11:26:32 +0300

    merge + bubble sort done

--- sortingAlgorithms.cpp
+++ sortingAlgorithms.cpp
@@ -47,9 +47,81 @@ int selectionSort(vector<T>& arr){
 }
 
 //BUBBLE SORT
+    template<typename T>
+    void bubble_sort(vector<T> &arr) {
+        int n = arr.size();
+        bool sorted = true;  // to check if the array is already sorted
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    swap(arr[j], arr[j + 1]);
+                    sorted = false;
+                }
+            }
+            if (sorted == true) // if reached this, then the array is already sored
+                break;
+        }
+    }
+
 
 //SHELL SORT
 
 //MERGE SORT
+    template<typename T>
+    void merge(vector<T> &arr, int l, int r, int mid) {
+        int i, j, k;
+        int sz1 = mid - l + 1; // in case of odd array, put the extra on the left sub array
+        int sz2 = r - mid; // size of right/left subarrays
+        int *subL = new int[sz1]; // left subarray, as dynamic array
+        int *subR = new int[sz2]; // Right subarray. as dynamic array (if static, the sz1,sz2 must be const)
+
+        for (int i = 0; i < sz1; i++) {
+            subL[i] = arr[l + i];  //insert element for left subarray
+        }
+        for (int j = 0; j < sz2; j++) {
+            subR[j] = arr[mid + 1 + j];  //insert elements for right subarray
+        }
+
+        i = j = 0;
+        k = l;
+        // start comparing
+        while (i < sz1 && j < sz2) // compare between right and left subarrays
+        {
+            if (subL[i] <= subR[j]) {
+                arr[k] = subL[i];
+                i++;
+            } else {
+                arr[k] = subR[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < sz1) //when the comarison is done but there re still elements on the left/right subarray, put the remaining element as it is
+        {
+            arr[k] = subL[i];
+            i++;
+            k++;
+        }
+        while (j < sz2) {
+            arr[k] = subR[j];
+            j++;
+            k++;
+        }
+    }
+
+    template<typename T>
+    void merge_sort(vector<T> &arr, int l, int r) {
+        if (l < r)
+        {
+            int mid = (l + r)  / 2;
+            merge_sort<T>(arr, l, mid);
+            merge_sort<T>(arr, mid + 1, r);
+            merge<T>(arr, l, r, mid);
+        }
+    }
 
 //QUICK SORT
